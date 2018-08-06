44b7791a26a1525925ea38343a1547bcbe72b7e1
Martin Francis <martin@classaxe.com>
2018-08-06 18:21:20 -0400

    1.0.1: Standalone install version for deploying on non-vagrant servers

---
+++ config_php.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Build=true when installing on a live unknown server
+
+config_php=(
+#  'version    shortname  port  build'
+   '5.4.45     5.4        9004  true'
+   '5.5.38     5.5        9005  true'
+   '5.6.35     5.6        9006  true'
+   '7.0.29     7.0        9007  true'
+   '7.1.16     7.1        9008  true'
+   '7.2.4      7.2        9009  true'
+)
+
+web_user='apache';
+web_group='apache';
